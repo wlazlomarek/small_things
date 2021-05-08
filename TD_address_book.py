c59606a259d07d230959593e961a9cecf1b784de
Marek Wlazlo <55976551+wlazlomarek@users.noreply.github.com>
2021-05-08 13:55:36 +0200

    Refactoring

--- TD_address_book.py
+++ TD_address_book.py
@@ -1,6 +1,7 @@
 import csv
+import sys
 
-def increment_and_write(email_list, index=0):
+def create_csv(email_list, index=1):
     with open(f'TB_address_newsletter_{index}.csv', 'w', newline='') as file:
         fieldnames = ['name', 'surname', 'display_name', 'nickname', 'email']
         writer = csv.DictWriter(file, fieldnames=fieldnames)
@@ -10,31 +11,29 @@ def increment_and_write(email_list, index=0):
             writer.writerow({'email': f'{el}'})
 
 
-with open('address.txt', 'r') as f:
-    emails = f.read().splitlines()
-    emails = [el.strip() for el in emails]
-    print(len(emails))
+def read_emails():
+    try:
+        with open('maile.txt', 'r') as f:
+            emails = f.read().splitlines()
+            emails = [el.strip() for el in emails]
+            return emailsa
+    except FileNotFoundError as fnf:
+        sys.exit(fnf)
 
-first_list = []
-second_list = []
-third_list = []
 
-if len(emails) > 49:
-    i = 0
-    for _ in emails:
-        if i < 50:
-            first_list.append(_)
-            i += 1
-        elif 50 <= i <= 99:
-            second_list.append(_)
-            i += 1
-        else:
-            third_list.append(_)
-            i += 1
-    increment_and_write(first_list, index=1)
-    increment_and_write(second_list, index=2)
-    increment_and_write(third_list, index=3)
-else:
-    increment_and_write(emails, index=1)
+def main():
+    i = 1
+    email_list = read_emails() 
 
-print('Done!')
+    while len(email_list) > 49:
+        temp_list = email_list[:49]
+        create_csv(temp_list, i)
+        for el in temp_list:
+            email_list.remove(el)
+        i += 1
+    else:
+        create_csv(email_list)
+
+
+if __name__ == '__main__':
+    main()
